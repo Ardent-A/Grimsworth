053fb8db6b3dea03f57b50315cb86a6cf673f9f6
Ardent-A <97244505+Ardent-A@users.noreply.github.com>
2023-07-21 20:42:03 +0800

    Added navigation

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 let effect = document.getElementById("effect")
 let trigger = document.getElementById("trigger")
-const toggleBtn = document.querySelector(".sidebar-toggle");
-const closeBtn = document.querySelector(".close-btn");
-const sidebar = document.querySelector(".sidebar");
-const html = document.getElementById("html");
-const overlay = document.getElementById("overlay");
+const toggleBtn = document.querySelector(".sidebar-toggle")
+const closeBtn = document.querySelector(".close-btn")
+const sidebar = document.querySelector(".sidebar")
+const html = document.getElementById("html")
+const overlay = document.getElementById("overlay")
+const bars = document.querySelector(".fa-bars")
+const checkpoint = 380;
+const webCheckpoint = 150;
+
+let color
 
 // Start of sidebar
 
 toggleBtn.addEventListener("click", function () {
-    sidebar.classList.add("show-sidebar");
+    sidebar.classList.add("show-sidebar")
     overlay.classList.add("overlay-on")
 });
 
 overlay.addEventListener("click", function () {
     sidebar.classList.remove("show-sidebar")
-    overlay.classList.remove("overlay-on");
+    overlay.classList.remove("overlay-on")
+})
+
+sidebar.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar")
+    overlay.classList.remove("overlay-on")
 })
 
 // html.addEventListener("click", function () {
@@ -31,7 +41,23 @@ function triggerEffect() {
     effect.classList.toggle("effects")
 }
 
-console.log(overlay)
+// Scroll Nav
+window.addEventListener("scroll", () => {
+    const currentScroll = window.scrollY;
+    if (currentScroll < checkpoint) {
+        backgroundColor = "none";
+    } else {
+        backgroundColor = "rgba(255, 255, 255, 0.7)";
+    }
+    toggleBtn.style.background = backgroundColor;
+    if (currentScroll < webCheckpoint) {
+        sidebar.classList.remove("scroll-background")
+    } else {
+        sidebar.classList.add("scroll-background")
+    }
+});
+
+
 
 // function removeEffect() {
 //     effect.classList.remove("effects")
